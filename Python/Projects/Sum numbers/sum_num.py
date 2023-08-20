e571805707da36a6e11d80165a0aba16eefb2ea8
npmaccto <n.phantawee@gmail.com>
2023-08-20 16:49:03 +0700

    update

--- Python/Projects/Sum numbers/sum_num.py
+++ Python/Projects/Sum numbers/sum_num.py
@@ -1,4 +1,5 @@
 from itertools import permutations,product
+from random import shuffle
 
 import pandas as pd
 
@@ -6,5 +7,6 @@ if __name__ == "__main__":
     list_a = [n for n in range(11)]
     # output = permutations(list_a, 2)
     output = [p for p in product(list_a, repeat=2)] # Cartesian product
+    shuffle(output)
     df = pd.DataFrame(list(output),columns=['val1','val2'])
     df.to_excel('./output.xlsx',index=False,engine='openpyxl')
